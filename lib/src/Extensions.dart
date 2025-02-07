3b89af52ec19df9e3f1c50f6a931c292d52a7a26
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-02-07 11:34:07 +0530

    firestore package search tag added

--- lib/src/Extensions.dart
+++ lib/src/Extensions.dart
@@ -13,12 +13,31 @@ extension DocExtension on Doc {
   static final String _baseDB = 'https://console.firebase.google.com/project/${FirestorepackageConfig.productID}/firestore/databases/-default-/data/~2F';
 
   ///Firestore link of the collection of the document
-  String get collectionLink => '$_baseDB${docPath.collection}';
+  String get collectionLink => exists ? '$_baseDB${docPath.collection}' : _baseDB;
 
   ///Firestore link of the document
   String get documentLink {
-    final String c = collectionLink;
-    final String d = docPath.id.replaceAll('+', '%2B');
-    return '$c~2F$d';
+    if (exists) {
+      final String c = collectionLink;
+      final String d = docPath.id.replaceAll('+', '%2B');
+      return '$c~2F$d';
+    } else {
+      return _baseDB;
+    }
+  }
+}
+
+extension QueryExtension on Queryy {
+  ///To simplify the queries accross any projects
+  Queryy applySearch(final List<String>? tags) {
+    late List<String> _tags = tags ?? const <String>[];
+    if (_tags.isNotEmpty) {
+      if (_tags.length > 9) {
+        _tags = _tags..removeRange(9, _tags.length - 1);
+      }
+      return where('tags', arrayContainsAny: _tags);
+    } else {
+      return this;
+    }
   }
 }
